5caadd5002fe1688cdac02e4feb10e165b959b23
Kaushal Jalan <47274860+kaushaljalan@users.noreply.github.com>
2020-11-24 14:31:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;//1-6
+
+var randomImage1="dice"+randomNumber1+".png";//dice1.png-dice6.png
+
+var randomImageSource1="images/"+randomImage1;//images/dice1.png-images/dice6.png
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource1);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;//1-6
+
+var randomImageSource2="images/"+"dice"+randomNumber2+".png";
+
+var image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImageSource2);
+
+
+if(randomNumber1>randomNumber2)
+  document.querySelector("h1").innerHTML="🚩Player1 Wins!";
+else if(randomNumber2>randomNumber1)
+  document.querySelector("h1").innerHTML="Player2 Wins!🚩"
+else
+  document.querySelector("h1").innerHTML="Draw!";
